b332f0b39ebd6993d258efa9efa86855543584f9
Pedro Carvalho <113921418+opedrocarvalho@users.noreply.github.com>
2024-05-16 18:28:12 -0300

    Update na estrutura do banco de dados

--- Script de criação do banco de dados RH.sql
+++ Script de criação do banco de dados RH.sql
@@ -47,22 +47,22 @@ CREATE TYPE "tipo_beneficiado" AS ENUM (
 
 CREATE TABLE "endereco" (
   "id_endereco" serial PRIMARY KEY,
-  "cep" varchar NOT NULL,
-  "logradouro" varchar,
+  "cep" varchar(10) NOT NULL,
+  "logradouro" varchar(255),
   "numero" int NOT NULL,
-  "complemento" varchar,
-  "bairro" varchar,
-  "cidade" varchar NOT NULL,
-  "estado" varchar NOT NULL
+  "complemento" varchar(255),
+  "bairro" varchar(100),
+  "cidade" varchar(100) NOT NULL,
+  "estado" varchar(2) NOT NULL
 );
 
 CREATE TABLE "usuario" (
   "id_usuario" serial PRIMARY KEY,
   "responsavel" integer,
-  "nome_completo" varchar NOT NULL,
-  "telefone" varchar NOT NULL,
-  "email" varchar,
-  "naturalidade" varchar,
+  "nome_completo" varchar(255) NOT NULL,
+  "telefone" varchar(20) NOT NULL,
+  "email" varchar(255),
+  "naturalidade" varchar(100),
   "genero" genero,
   "data_nascimento" date NOT NULL,
   "id_endereco" integer,
@@ -72,19 +72,19 @@ CREATE TABLE "usuario" (
 
 CREATE TABLE "empresa" (
   "id_empresa" serial PRIMARY KEY,
-  "nome_empresa" varchar NOT NULL,
-  "cnpj" varchar NOT NULL,
-  "telefone" varchar,
-  "email" varchar,
+  "nome_empresa" varchar(255) NOT NULL,
+  "cnpj" varchar(18) NOT NULL,
+  "telefone" varchar(20),
+  "email" varchar(255),
   "id_endereco" integer,
   FOREIGN KEY ("id_endereco") REFERENCES "endereco" ("id_endereco")
 );
 
 CREATE TABLE "cargo" (
   "id_cargo" serial PRIMARY KEY,
-  "descricao" varchar,
+  "descricao" varchar(255),
   "salario" float NOT NULL,
-  "hierarquia" varchar NOT NULL,
+  "hierarquia" varchar(100) NOT NULL,
   "carga_horaria" interval
 );
 
@@ -92,7 +92,7 @@ CREATE TABLE "funcionario" (
   "matricula" serial PRIMARY KEY,
   "data_admissao" date NOT NULL,
   "data_demissao" date,
-  "motivo_demissao" varchar,
+  "motivo_demissao" varchar(255),
   "id_empresa" integer,
   "id_cargo" integer,
   "id_usuario" integer,
@@ -122,7 +122,7 @@ CREATE TABLE "documento" (
   "id_documento" serial PRIMARY KEY,
   "id_usuario" integer,
   "tipo" tipo_documento,
-  "numero" varchar NOT NULL,
+  "numero" varchar(50) NOT NULL,
   "emissao" date NOT NULL,
   "validade" date NOT NULL,
   FOREIGN KEY ("id_usuario") REFERENCES "usuario" ("id_usuario")
@@ -131,8 +131,8 @@ CREATE TABLE "documento" (
 CREATE TABLE "setor" (
   "id_setor" serial PRIMARY KEY,
   "matricula" integer,
-  "nome_setor" varchar NOT NULL,
-  "responsavel" varchar NOT NULL,
+  "nome_setor" varchar(100) NOT NULL,
+  "responsavel" varchar(100) NOT NULL,
   "id_empresa" integer,
   FOREIGN KEY ("id_empresa") REFERENCES "empresa" ("id_empresa"),
   FOREIGN KEY ("matricula") REFERENCES "funcionario" ("matricula")
@@ -144,7 +144,7 @@ CREATE TABLE "contra_cheque" (
   "bonus" float,
   "pessoa" tipo_pessoa NOT NULL,
   "desconto" float,
-  "razao_desconto" varchar,
+  "razao_desconto" varchar(255),
   "data_pagamento" date NOT NULL,
   FOREIGN KEY ("matricula") REFERENCES "funcionario" ("matricula")
 );
@@ -154,18 +154,18 @@ CREATE TABLE "registro_horas" (
   "matricula" integer,
   "data" date NOT NULL,
   "hora_entrada" time NOT NULL,
-  "hora_saida" time NOT NULL,
+  "hora_saida" time,
   FOREIGN KEY ("matricula") REFERENCES "funcionario" ("matricula")
 );
 
 CREATE TABLE "avaliacao" (
   "id_avaliacao" serial PRIMARY KEY,
   "matricula" integer,
-  "avaliador" varchar NOT NULL,
+  "avaliador" varchar(100) NOT NULL,
   "data_avaliacao" date NOT NULL,
   "nota" integer NOT NULL,
-  "competencia" varchar NOT NULL,
-  "comentarios" varchar,
+  "competencia" varchar(100) NOT NULL,
+  "comentarios" varchar(255),
   FOREIGN KEY ("matricula") REFERENCES "funcionario" ("matricula")
 );
 
